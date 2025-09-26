dad98f51c4817484c3f73bf6331192b3fc51bd28
Dolganov Alexander <Tyuiu.DolganovAV@outlook.com>
2025-09-26 12:24:11 +0500

    Add project files.

---
+++ Tyuiu.DolganovAV.Sprint2.Task0.V10.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.DolganovAV.Sprint2.Task0.V10.Lib;
+namespace Tyuiu.DolganovAV.Sprint2.Task0.V10.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetCompareOperations()
+        {
+            DataService ds = new DataService();
+            int x = 1305;
+            int y = 475;
+            bool[] res = new bool[6];
+            res = ds.GetCompareOperations(x, y);
+            bool[] wait = new bool[6] { false, true, false, true, false, true };
+            CollectionAssert.AreEqual(res, wait);
+        }
+    }
+}

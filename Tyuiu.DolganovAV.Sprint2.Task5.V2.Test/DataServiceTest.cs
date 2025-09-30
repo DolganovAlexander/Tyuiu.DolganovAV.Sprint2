8ef86c3a540b05cb9a3d98de2313950432169440
Dolganov Alexander <Tyuiu.DolganovAV@outlook.com>
2025-09-30 14:47:54 +0500

    Task5

---
+++ Tyuiu.DolganovAV.Sprint2.Task5.V2.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.DolganovAV.Sprint2.Task5.V2.Lib;
+namespace Tyuiu.DolganovAV.Sprint2.Task5.V2.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidFindMonthSeason()
+        {
+            DataService ds = new DataService();
+            int value = 5;
+            Assert.AreEqual("Весна", ds.FindMonthSeason(value));
+        }
+    }
+}
---
+++ Tyuiu.DolganovAV.Sprint2.Task5.V2/Program.cs
@@ -0,0 +1,44 @@
+﻿using Tyuiu.DolganovAV.Sprint2.Task5.V2.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #2 | Выполнил: Долганов А. В. | ПИНб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #2                                                               *");
+        Console.WriteLine("* Тема: Оператор switch                                                   *");
+        Console.WriteLine("* Задание #5                                                              *");
+        Console.WriteLine("* Вариант #2                                                              *");
+        Console.WriteLine("* Выполнил: Долганов Александр Витальевич | ПИНб-25-1                     *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая использует оператор switch, вычисляет       *");
+        Console.WriteLine("* требуемое значение и возвращает результат.                              *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int month;
+        string res;
+        Console.WriteLine("Введите номер месяца: ");
+        month = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        if ((month < 1) || (month > 12)) 
+        {
+           res = "Введенно неверное значение";
+        }
+        else
+        {
+           res = $"Данный месяц: {ds.FindMonthSeason(month)}";
+        }
+        Console.WriteLine(res);
+        Console.ReadKey();
+    }   
+}

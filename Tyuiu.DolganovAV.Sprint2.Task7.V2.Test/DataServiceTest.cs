3393b24ec3586dcbbe38b44f078d7cd0f7e3c2f2
Dolganov Alexander <Tyuiu.DolganovAV@outlook.com>
2025-10-04 00:53:13 +0500

    Task7

---
+++ Tyuiu.DolganovAV.Sprint2.Task7.V2.Test/DataServiceTest.cs
@@ -0,0 +1,28 @@
+﻿using Tyuiu.DolganovAV.Sprint2.Task7.V2.Lib;
+namespace Tyuiu.DolganovAV.Sprint2.Task7.V2.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCheckDotInShadedArea1()
+        {
+            DataService ds = new DataService();
+            double x = 0.5;
+            double y = -0.5;
+            bool wait = true;
+            var res = ds.CheckDotInShadedArea(x, y);
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void ValidCheckDotInShadedArea2()
+        {
+            DataService ds = new DataService();
+            double x = 0.5;
+            double y = 0.7;
+            bool wait = false;
+            var res = ds.CheckDotInShadedArea(x, y);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.DolganovAV.Sprint2.Task7.V2/Program.cs
@@ -0,0 +1,45 @@
+﻿using Tyuiu.DolganovAV.Sprint2.Task7.V2.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #2 | Выполнил: Долганов А. В. | ПИНб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #2                                                               *");
+        Console.WriteLine("* Тема: Добавление к решению итоговых проектов к спринту                  *");
+        Console.WriteLine("* Задание #7                                                              *");
+        Console.WriteLine("* Вариант #2                                                              *");
+        Console.WriteLine("* Выполнил: Долганов Александр Витальевич | ПИНб-25-1                     *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая запрашивает исходные данные и вычисляет,    *");
+        Console.WriteLine("* находится ли точка с координатами X, Y в заштрихованной области.        *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Введите X:");
+        double x = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите Y:");
+        double y = Convert.ToDouble(Console.ReadLine());
+        
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        if (ds.CheckDotInShadedArea(x,y) == true)
+        {
+            Console.WriteLine($"Точка находится в закрашенной области ({ds.CheckDotInShadedArea(x, y)})");
+        }
+        else
+        {
+            {
+                Console.WriteLine($"Точка не находится в закрашенной области ({ds.CheckDotInShadedArea(x, y)})");
+            }
+        }
+    }
+}
